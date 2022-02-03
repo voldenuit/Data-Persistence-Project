d1c7ab821cdecba4ac22052544eedf9e565f7b63
voldenuit <voldenuit.deuche@gmail.com>
2022-02-03 23:11:35 +0100

    add QuitGame() handler
    used for exiting the game in editor and after build

--- Assets/Scripts/UIManagerS.cs
+++ Assets/Scripts/UIManagerS.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class UIManagerS : MonoBehaviour
 {
@@ -14,6 +17,15 @@ public class UIManagerS : MonoBehaviour
     public void StartGame()
     {
       SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+      #if UNITY_EDITOR
+      EditorApplication.ExitPlaymode();
+      #else
+      Application.Quit();
+      #endif
     }
 
 
